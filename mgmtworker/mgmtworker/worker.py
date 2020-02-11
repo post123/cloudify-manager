022efe8e45a86c51e0159c5f3d65a865e4711706
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2020-02-11 22:53:53 +0100

    not never

--- mgmtworker/mgmtworker/worker.py
+++ mgmtworker/mgmtworker/worker.py
@@ -37,7 +37,7 @@ logger = logging.getLogger('mgmtworker')
 class CloudifyWorkflowConsumer(TaskConsumer):
     routing_key = 'workflow'
     handler = dispatch.WorkflowHandler
-    late_ack = 'never'
+    late_ack = True
 
     def _print_task(self, ctx, action, status=None):
         if ctx['type'] in ['workflow', 'hook']:
