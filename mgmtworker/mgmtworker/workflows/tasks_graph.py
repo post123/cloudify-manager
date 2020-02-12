8f12f3f097af3003b82e68270122052987fff509
Łukasz Maksymczuk <lukasz@lukasz-maksymczuk.intra.codilime.com>
2020-02-12 17:48:02 +0100

    remove log

--- mgmtworker/mgmtworker/workflows/tasks_graph.py
+++ mgmtworker/mgmtworker/workflows/tasks_graph.py
@@ -245,9 +245,6 @@ class TaskDependencyGraph(object):
             aws = [task.data.async_result
                  for task in current
                  if task is not None]
-            logger.info('aws:')
-            for task in current:
-                logger.info('    %s %s: %s', task.data, task.data.__class__, task.data.async_result)
             done, pending = await asyncio.wait(
                 aws,
                 return_when=asyncio.FIRST_COMPLETED
